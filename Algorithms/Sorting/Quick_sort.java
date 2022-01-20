919d96ff7c9c1e0234db6c197d44328cc77c8d78
f18bc <42793728+f18bc@users.noreply.github.com>
2022-01-20 14:12:42 -0500

    Update Quick_sort.java

--- Algorithms/Sorting/Quick_sort.java
+++ Algorithms/Sorting/Quick_sort.java
@@ -78,3 +78,38 @@ class QuickSort {
  }
 }
 */
+
+/*
+class Solution {
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length-1);
+        return nums;
+    }
+    public void quickSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int pi = partition(nums, left, right);
+            quickSort(nums, left, pi - 1);
+            quickSort(nums, pi + 1, right);
+        }
+    }
+    public int partition(int[] nums, int left, int right) {
+        int pivotIndex = left + new Random().nextInt(right - left + 1);
+        int pivot = nums[pivotIndex];
+        swap(nums, pivotIndex, right);
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (nums[j] <= pivot) {
+                i++;
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, i + 1, right);
+        return i + 1;
+    }
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+*/
